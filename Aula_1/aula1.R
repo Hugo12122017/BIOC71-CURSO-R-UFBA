25a53c45cfe201c88f9de93064db1e9b007dae4d
hugo12122017 <hugoandrade915@gmail.com>
2017-12-12 18:36:42 -0200

    script1

---
+++ Aula_1/aula1.R
@@ -0,0 +1,90 @@
+1+1
+79*90
+115.7*73 + 4^2
+2x= 17-13
+x= (17-13)/ 2
+x^2
+x= (17-13)/ 2 #vc escreve observações e elas nao sao uasadas como scripts
+sqrt(x)
+log10
+?log
+sqrt(x)
+log(x, base= 3)
+log(exp(3))
+log10(1e7) # = 7
+??linear
+??canonica
+??modularity
+??MANOVA
+library(logical.return=)
+library(httr)
+?cluster_lowain
+library(anova)
+library(datasets)
+??datasets
+??MANOVA
+??analyses
+Sorvete = "feijao"
+sorvete = x
+sorvete = sorvete + 8
+geladeira = c("sorvete", "cebola", "agua", "miojo", "macarrão", "ketchup")
+geladeira = c(geladeira, "pizza")
+geladeira = c(8, 9, 15, "salada")
+numeros = c(8, 9 , 15)
+numeros
+1:10
+help(seq)
+??seq
+?seq
+seq(from=1,to=100,by=3)
+seq(from=1,to=10,by=0.5)
+rep("chuchu", 3)
+rep(80, 5)
+peso.real = c(100, 45, 77)
+peso.no.perfil = c(70, 50)
+qiolos.omitido
+quilos.omitidos = peso.real - peso.no.perfil
+quilos.omitido
+qiolos.omitido =
+quilos.omitid
+peso.real2 = c(100, 45, 77, 60, 50, 77, 5)
+length(peso.real2)
+peso.real2[5]
+vetor1=LETTERS
+vetor2=1:26
+dados=data.frame(x=vetor1,y=vetor2)
+vetor3=letters
+vetor4=26:1
+nomescolunas= c("Letras", "Numeros")
+dadosNomeados=data.frame(vetor3,y= vetor4)
+colnames(dadosNomeados) = nomeascolunas
+colnames(dadosNomeados) = nomescolunas
+head(dadosNomeados)
+dadosNomeados[1,2]
+dadosNomeados[5,1]
+dadosNomeado$Numeros
+dadosNomeados$Numeros[4]
+Idade = c(3.0, 4.0, 5.0, 6.0, 8.0, 9.0, 10.0, 11.0, 12.0, 14.0, 15.0, 16.0, 17.0)
+Idade[c(5, 4, 1, 10, 12)]
+Idade > 6
+Idade >= 6
+Idade[ Idade> 6 ]
+
+
+getwd()
+getwd
+getwd()
+
+
+
+aves = c(22, 28, 30, 35, 44, 56)
+length(aves)
+max(aves)
+min(aves)
+aves = c(min, max)
+sum(aves)
+aves = c(22, 28, 30, 35, 44, 56)
+sum(aves)
+aves^2
+aves/10
+
